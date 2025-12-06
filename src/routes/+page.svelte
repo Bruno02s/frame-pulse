e74f6d39c7b4b3cb48d929c73f39c95b9a414680
Bruno02s <brunolourenco02s@gmail.com>
2025-12-06 10:49:14 -0300

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,19 +11,19 @@ pages: [
 { id: 0, imageUrl: '/images/manga/mil-tsurus-capa.png' },
 { 
 id: 1, 
-sounds: [
-{ url: '/sounds/peace.ogg', volume: 0.5, loop: true }
-],
-frames: [
-{ id: 'tl_1', imageUrl: '/images/manga/pg1_tl1.png', imagePosition: { left: 50, top: 5, size: 43 } },
-{ id: 'qd_1', imageUrl: '/images/manga/pg_1_qd_1.png', imagePosition: { left: 25, top: 27, size: 43 } },
-{ id: 'qd_2', imageUrl: '/images/manga/pg_1_qd_2.png', imagePosition: { left: 72, top: 28.70, size: 50 } },
-{ id: 'qd_3', imageUrl: '/images/manga/pg_1_qd_3.png', imagePosition: { left: 25, top: 60, size: 44 }, sounds: [{ url: '/sounds/childs.wav', volume: 0.7 }] },
-{ id: 'qd_4', imageUrl: '/images/manga/pg_1_qd_4.png', imagePosition: { left: 74, top: 62.50, size: 51 } },
-{ id: 'qd_5', imageUrl: '/images/manga/pg_1_qd_5.png', imagePosition: { left: 50, top: 85, size: 94.70 }, elements: [
-{ id: 'el_1', imageUrl: '/images/manga/pg_1_qd_5_el1.png', left: 12, top: 38.50, width: 14, height: 18, animation: { type: 'float', intensity: 4.0, duration: 5 } },
-{ id: 'el_2', imageUrl: '/images/manga/pg_1_qd_5_el2.png', left: 30, top: 52, width: 12, height: 12, animation: { type: 'float', intensity: 2.7, duration: 6 } }
-] }
+  sounds: [
+  { url: '/sounds/peace.ogg', volume: 0.5, loop: true }
+  ],
+  frames: [
+    { id: 'tl_1', imageUrl: '/images/manga/pg1_tl1.png', imagePosition: { left: 50, top: 5, size: 43 } },
+    { id: 'qd_1', imageUrl: '/images/manga/pg_1_qd_1.png', imagePosition: { left: 25, top: 27, size: 43 } },
+    { id: 'qd_2', imageUrl: '/images/manga/pg_1_qd_2.png', imagePosition: { left: 72, top: 28.70, size: 50 } },
+    { id: 'qd_3', imageUrl: '/images/manga/pg_1_qd_3.png', imagePosition: { left: 25, top: 60, size: 44 }, sounds: [{ url: '/sounds/childs.wav', volume: 0.7 }] },
+    { id: 'qd_4', imageUrl: '/images/manga/pg_1_qd_4.png', imagePosition: { left: 74, top: 62.50, size: 51 } },
+    { id: 'qd_5', imageUrl: '/images/manga/pg_1_qd_5.png', imagePosition: { left: 50, top: 85, size: 94.70 }, elements: [
+      { id: 'el_1', imageUrl: '/images/manga/pg_1_qd_5_el1.png', left: 12, top: 38.50, width: 14, height: 18, animation: { type: 'float', intensity: 4.0, duration: 5 } },
+      { id: 'el_2', imageUrl: '/images/manga/pg_1_qd_5_el2.png', left: 30, top: 52, width: 12, height: 12, animation: { type: 'float', intensity: 2.7, duration: 6 } }
+  ] }
 ] 
 },
 { 
